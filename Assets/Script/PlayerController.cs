39429ecafba6fa34e70c76305cd8ce7d5a813c3a
e1drah <113189459+e1drah@users.noreply.github.com>
2022-09-20 12:32:12 -0300

    Finished second level layout

--- Assets/Script/PlayerController.cs
+++ Assets/Script/PlayerController.cs
@@ -7,15 +7,32 @@ using TMPro;
 public class PlayerController : MonoBehaviour
 {
     public float speed = 0;
+
     public TextMeshProUGUI countText;
+
     public GameObject winTextObject;
     public GameObject Player;
-    public Vector3 SpawnPoint;
+    //Spawnpoints
+    public GameObject spawnPoint1;
+    public GameObject spawnPoint2;
+    public GameObject SpawnPoint3;
+    public GameObject SpawnPoint4;
+    public GameObject SpawnPoint5;
+    public GameObject SpawnPoint6;
+    public GameObject SpawnPoint7;
+    public GameObject SpawnPoint8;
+    public GameObject SpawnPoint9;
+    public GameObject SpawnPoint10;
+
 
     private Rigidbody rb;
-    private int count;
+
     private float movementX;
     private float movementY;
+
+    private int count;
+    private int lvlCount;
+    private int keyCount;
     
     // Start is called before the first frame update
     void Start()
@@ -57,9 +74,34 @@ public class PlayerController : MonoBehaviour
 
             SetCountText();
         }
+        if (other.gameObject.CompareTag("End"))
+        {
+            lvlCount += 1;
+        }
         if (other.gameObject.CompareTag("Respawn"))
         {
-            Player.gameObject.transform.position = SpawnPoint;
+            if (lvlCount == 0)
+            {
+            Player.gameObject.transform.position = spawnPoint1.gameObject.transform.position;
+            }
+            if(lvlCount == 1)
+            {
+                Player.gameObject.transform.position = spawnPoint2.gameObject.transform.position;
+            }
+
+        }
+        if (other.gameObject.CompareTag("nextlvl"))
+        {
+            if (lvlCount == 0)
+            {
+                Player.gameObject.transform.position = spawnPoint1.gameObject.transform.position;
+            }
+            if (lvlCount == 1)
+            {
+
+                Player.gameObject.transform.position = spawnPoint2.gameObject.transform.position;
+            }
+
         }
     }
 }
