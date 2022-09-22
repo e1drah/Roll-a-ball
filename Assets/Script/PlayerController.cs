1c2c4aee55ce71cdd91e965e98665511c741d7cb
e1drah <113189459+e1drah@users.noreply.github.com>
2022-09-22 15:39:48 -0300

    Fished level 2

--- Assets/Script/PlayerController.cs
+++ Assets/Script/PlayerController.cs
@@ -9,9 +9,12 @@ public class PlayerController : MonoBehaviour
     public float speed = 0;
 
     public TextMeshProUGUI countText;
+    public TextMeshProUGUI keytext;
+    public TextMeshProUGUI Timer;
+    public TextMeshProUGUI levelCount;
 
-    public GameObject winTextObject;
     public GameObject Player;
+    public GameObject Key;
     //Spawnpoints
     public GameObject spawnPoint1;
     public GameObject spawnPoint2;
@@ -24,6 +27,19 @@ public class PlayerController : MonoBehaviour
     public GameObject SpawnPoint9;
     public GameObject SpawnPoint10;
 
+    //Amount of keys needed to compleate the level
+    public int lvl1Keys;
+    public int lvl2Keys;
+    public int lvl3Keys;
+    public int lvl4Keys;
+    public int lvl5Keys;
+    public int lvl6Keys;
+    public int lvl7Keys;
+    public int lvl8Keys;
+    public int lvl9Keys;
+    public int lvl10Keys;
+
+    public int bounce; 
 
     private Rigidbody rb;
 
@@ -31,18 +47,18 @@ public class PlayerController : MonoBehaviour
     private float movementY;
 
     private int count;
-    private int lvlCount;
+    private int lvlCount = 1;
     private int keyCount;
     
     // Start is called before the first frame update
     void Start()
     {
+        keyCount = lvl1Keys;
         rb = GetComponent<Rigidbody>();
         count = 0;
-        SetCountText();
-        winTextObject.SetActive(false);
+        SetHudText();
     }
-
+    //Moves the player
     void OnMove(InputValue movementValue)
     {
         Vector2 movementVector = movementValue.Get<Vector2>();
@@ -50,15 +66,15 @@ public class PlayerController : MonoBehaviour
         movementX = movementVector.x;
         movementY = movementVector.y;
     }
-
-    void SetCountText()
+     
+    //counts how many cubes the player collected 
+    void SetHudText()
     {
-        countText.text = "Count:" + count.ToString();
-        if (count >= 28)
-        {
-            winTextObject.SetActive(true);
-        }
+        levelCount.text = "Level: " + lvlCount.ToString();
+        keytext.text = "Keys left: " + keyCount.ToString();
+        countText.text = "Count: " + count.ToString();
     }
+    //moves the player
     void FixedUpdate()
     {
         Vector3 movement = new Vector3(movementX,0.0f, movementY);
@@ -67,41 +83,62 @@ public class PlayerController : MonoBehaviour
     }
     private void OnTriggerEnter(Collider other)
     {
+        //player pick up an item
         if (other.gameObject.CompareTag("Pickup"))
         {
             other.gameObject.SetActive(false);
             count += 1;
 
-            SetCountText();
+            SetHudText();
         }
+        //counting which level the player will respawn to
         if (other.gameObject.CompareTag("End"))
         {
             lvlCount += 1;
         }
+        //Respawning the player
         if (other.gameObject.CompareTag("Respawn"))
         {
-            if (lvlCount == 0)
-            {
-            Player.gameObject.transform.position = spawnPoint1.gameObject.transform.position;
-            }
-            if(lvlCount == 1)
-            {
-                Player.gameObject.transform.position = spawnPoint2.gameObject.transform.position;
-            }
-
-        }
-        if (other.gameObject.CompareTag("nextlvl"))
-        {
-            if (lvlCount == 0)
-            {
+            if (lvlCount == 1)
+            {   
                 Player.gameObject.transform.position = spawnPoint1.gameObject.transform.position;
             }
-            if (lvlCount == 1)
+            if(lvlCount == 2)
             {
-
                 Player.gameObject.transform.position = spawnPoint2.gameObject.transform.position;
             }
 
         }
+        //sends the player to the next lvl
+        if (other.gameObject.CompareTag("nextlvl"))
+        {
+            //if (lvlCount == 1)
+            //{
+            //    keyCount = lvl1Keys;
+            //    SetHudText();
+            //    Player.gameObject.transform.position = spawnPoint1.gameObject.transform.position;
+            //}
+            if (lvlCount == 2)
+            {
+                keyCount = lvl2Keys;
+                SetHudText();
+                Player.gameObject.transform.position = spawnPoint2.gameObject.transform.position;
+            }
+
+        }
+        //send the player up
+        if (other.gameObject.CompareTag("Bounce"))
+        {
+            rb.AddForce(Vector3.up * bounce);
+        }
+        //Player picking up the keys
+        if (other.gameObject.CompareTag("Key"))
+        {
+            other.gameObject.SetActive(false);
+            keyCount -= 1;
+
+            SetHudText();
+        }
+
     }
 }
